72f37ed5afc0c0067a7f42d30afc41363895abef
Jiří Špác <capajj@gmail.com>
2023-07-17 13:27:05 +0200

    a tiny sample GQL hello query

---
+++ server.ts
@@ -0,0 +1,48 @@
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { compileQuery, isCompiledQuery } from 'graphql-jit'
+
+const typeDefs = `
+type Query {
+  hello: String
+}
+`
+const resolvers = {
+  Query: {
+    hello() {
+      return new Promise((resolve) => setTimeout(() => resolve('World!'), 200))
+    }
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const query = `
+{
+  hello
+}
+`
+const { parse } = require('graphql')
+const document = parse(query)
+
+const compiledQuery = compileQuery(schema, document)
+// check if the compilation is successful
+
+if (!isCompiledQuery(compiledQuery)) {
+  console.error(compiledQuery)
+  throw new Error('Error compiling query')
+}
+
+const executionResult = await compiledQuery.query({}, {}, {})
+console.log(executionResult)
+
+// const server = Bun.serve({
+//   port: 3000,
+//   development: true,
+//   async fetch(request) {
+//     const body = request.body
+
+//     return new Response('Welcome to Bun!')
+//   }
+// })
+
+// console.log(`Listening on http://localhost:${server.port}`)
